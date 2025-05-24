a771e07dcc74d3b06182508d4bd459d0a9ada228
cjchichi <cynthia1973chichi@gmail.com>
2025-05-24 18:34:48 +0300

    add  Pipfile.lock         modified:   lib/functions.py

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,25 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
 
 def greet(name):
     pass
+    print(f"Hello, {name}!")
+    
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
     pass
+    result = num1 + num2
+    assert result == num1 + num2
+    return result
 
 def halve(number):
     pass
+    result = number / 2
+    assert result == number / 2
+    return result
